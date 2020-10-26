a87f6603dcd10a800c8ce11106ff0971595d4e8e
Sara Ward <saraward@MacBook-Pro-6.local>
2020-10-26 15:27:38 -0400

    dropdown fixed

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,24 +1,30 @@
-import React from "react";
-import Dropdown from "react-bootstrap/Dropdown";
+import React, { useState, useEffect } from "react";
+import Form from "react-bootstrap/Form";
+import "../scss/homepage.scss";
 
 function Home() {
   console.log("hello homePage");
+  // const [dropdownOpen, setDropdownOpen] = React.useState(false);
+  // const toggle = () => setDropdownOpen((prevState) => !prevState);
+
   return (
-    <>
+    <div className="homepage">
       <h1>It's all Gouda</h1>
-      <h3>Quarantine Food Picker</h3>
+      <h2>Quarantine Food Picker</h2>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwPonxJBPD62Mz4zFtjrxrEBTsrM2DaVbLdA&usqp=CAU"></img>
-      <Dropdown className="Dropdown">
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          How Ya Feelin'?
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Happy</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Sad</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Moody</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    </>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlSelect1">
+          <Form.Label>How you Feelin'?</Form.Label>
+          <br />
+          <Form.Control as="select">
+            <option>Happy</option>
+            <option>Sad</option>
+            <option>Moody</option>
+          </Form.Control>
+        </Form.Group>
+      </Form>
+      <br />
+    </div>
   );
 }
 
