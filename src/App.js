a1a71e8a3cafdec29ffc8b5e67ce2a0c288b592e
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-10-26 15:05:48 -0500

    added form.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Route, Link, Switch } from 'react-router';
-import Home from '../src/components/homePage'
-import Nav from '../src/components/navbar'
-import Footer from '../src/components/footer'
+import Home from '../src/components/homePage';
+import Nav from '../src/components/navbar';
+import Footer from '../src/components/footer';
+import Form from '../src/components/form';
 
 function App() {
 	return (
-	<>
-		<Nav />
-		<Home />
-		<Footer />
-	</>
+		<>
+			<Nav />
+			<Home />
+			<Footer />
+			<Form />
+		</>
 	);
 }
 
