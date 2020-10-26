152e416bf5f258be3683b4e530ab1952363ee935
Sara Ward <saraward@MacBook-Pro-6.local>
2020-10-26 17:22:06 -0400

    Team members

--- src/components/team.js
+++ src/components/team.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Team = (props) => {
-	return <h2>about the team goes here</h2>;
+const Team = () => {
+  return (
+    <div>
+      <h2>Kilah</h2>;<img className="Kilah" src=""></img>
+      <p>words words more words, food puns</p>
+      <h2>Hannah</h2>
+      <img className="Hannah" src=""></img>
+      <p>words words more words, food puns</p>
+      <h2>Chase</h2>
+      <img className="Chase" src=""></img>
+      <p>words words more words, food puns</p>
+      <h2>Sara</h2>
+      <img className="Sara" src=""></img>
+      <p>words words more words, food puns</p>
+    </div>
+  );
 };
 
 export default Team;
