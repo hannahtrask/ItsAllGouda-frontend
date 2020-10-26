ac0268ec1c0847047d36edcb6061f4bfca09c1c8
Chase Wood <chase@Chases-MacBook-Pro.local>
2020-10-26 12:34:38 -0500

    made footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import react from 'react';
+import React from 'react';
 import { Route, Link, Switch } from 'react-router';
+import Footer from './components/footer';
 
 function App() {
-	return <h1>Hello World</h1>;
+	return (
+		<>
+			<h1>Hello World</h1>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
