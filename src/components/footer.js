a44418c1995260a3c269b0abdc642cc302ead09c
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-27 13:39:26 -0600

    random pun generator workinggit add .

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,23 +2,27 @@ import React, { useState, useEffect } from 'react';
 import '../scss/footer.scss';
 
 function Footer() {
-//instantiating state to randomize puns
 const [pun, setPun] = useState('Whatever you do, don\'t copy pasta.')
+const [data, setData] = useState([])
 
-//url for fetch call
 const url = 'https://cdn.contentful.com/spaces/i400pneoa60x/environments/master/entries?access_token=tqdo3TnhYgSsLQukoxBUvQQj_3vHkJRKG2_TpD-fVJo'
 
-// //this function will get a pun
-// const getPun = pun => {
-// 	fetch(url)
-// 	.then((res)=>res.json())
-// 	.then((data)=>setPun(data))
-// }
+useEffect(()=> {fetch(url)
+	.then((res)=>res.json())
+	.then((data)=>setData(data))
+	}, [] )
 
-// //this will run on mount
-// useEffect(()=>{
-// 	getPun()
-// }, [])
+let arr = data.items
+
+useEffect(()=> {
+if (Array.isArray(arr)) {
+	let index = arr[Math.floor(Math.random() * arr.length)];
+	let newPun = index.fields.pun;
+	console.log('this is newPun:', newPun);
+	setPun(newPun);
+ }
+})
+
 
 	return <div className='footer'>{pun}</div>;
 }
