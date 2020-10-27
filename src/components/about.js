db5739cdba687cb3eaa02f043b4f6c4f0e9503c7
Kilah Willingham <kilahmay@kilahs-air.lan>
2020-10-26 21:28:58 -0700

    added About copy

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = (props) => {
     return (
-        <h2>about the app goes here</h2>
+        <>
+            <h2>So... what is this?</h2>
+            <p>We'll cut to the cheese, It's All Gouda! is an interactive just-for-fun app to cure your quarantine blues. Feeling sassy, punchy, happy or mad? Tell us, and we'll offer a meal suggestion that'll make you laugh, roll your eyes, or make a run to your nearest store. Don't like what we plated? Add your own food with the name, pic and description of your choosing. <br /> Zoom'd out? Got a crush? Tired of your coworkers? Add the foods that match your mood. Or, lettuce souprise you! <br />Either way, It's All Gouda. Y'all bready for this?</p>
+        </>
     )
 }
 
