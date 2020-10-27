6743cff77656435fe1b5a70a8ef5d7b8537a246c
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-27 11:40:36 -0600

    url added, state instantiated, useEffect imported

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../scss/footer.scss';
 
 function Footer() {
-	return <div className='footer'>Funny Pun goes here</div>;
+//instantiating state to randomize puns
+const [pun, setPun] = useState('Whatever you do, don\'t copy pasta.')
+
+//url for fetch call
+const url = 'https://cdn.contentful.com/spaces/i400pneoa60x/environments/master/entries?access_token=tqdo3TnhYgSsLQukoxBUvQQj_3vHkJRKG2_TpD-fVJo'
+
+// //this function will get a pun
+// const getPun = pun => {
+// 	fetch(url)
+// 	.then((res)=>res.json())
+// 	.then((data)=>setPun(data))
+// }
+
+// //this will run on mount
+// useEffect(()=>{
+// 	getPun()
+// }, [])
+
+	return <div className='footer'>{pun}</div>;
 }
 
 export default Footer;
