252e681bf5dfb6377725c9af647cab88be5f339e
Sara Ward <saraward@MacBook-Pro-6.local>
2020-10-28 14:09:40 -0400

    update

--- src/components/team.js
+++ src/components/team.js
@@ -6,7 +6,12 @@ const Team = () => {
     <div className="chaseandgals">
       <h2 className="kilahname">Kilah</h2>
       <h3>Backend Baller</h3>
-      <img className="kilah" src="" />
+      <img
+        className="kilah"
+        src="https://i.ibb.co/9rDfC0m/Kilah-mask.png"
+        alt="Kilah-mask"
+        border="0"
+      />
       <p>words words more words, food puns</p>
       <h2 className="kilahname">Hannah</h2>
       <h3>Frontend Phenom</h3>
@@ -18,16 +23,21 @@ const Team = () => {
       <p>words words more words, food puns</p>
       <h2 className="kilahname">Sara</h2>
       <h3>Sassmaster</h3>
-      <img className="sara" src=""></img>
+      <img
+        className="sara"
+        src="https://i.ibb.co/xLjfm5s/Sara-mask.png"
+        alt="Sara-mask"
+        border="0"
+      />
       <p>
         Sara's passion has revolved around the music industry, graphic design
-        and festivals in the Atlanta area, and worldwide. She has always had a
-        curiosity for how computers and how machines work, with a borderline
-        compulsive urge to take video gear apart. Her time spent in the
-        immersive program at General Assembly was one of the best challenges of
-        2020. Learning a new skill that will allow her to grow more within the
-        entertainment industry as well as being able to enter a new "world" is
-        exactly the optimism and bright future the dull year needed.
+        and festivals in the Atlanta area. She has always had a curiosity for
+        how computers and how machines work, with a borderline compulsive urge
+        to take video gear apart. Her time spent in the immersive program at
+        General Assembly was one of the best challenges of 2020. Learning a new
+        skill that will allow her to grow more within the entertainment industry
+        as well as being able to enter a new "world" is exactly the optimism and
+        bright future the dull year needed.
       </p>
     </div>
   );
