1c78f7157c038f56742140124003e74ac0d07b8d
Hannah Catherine Trask <hctrask@gmail.com>
2020-10-28 08:40:05 -0600

    deleted files

--- src/components/displayFoods.js
+++ src/components/displayFoods.js
@@ -1,28 +1,46 @@
-import React from "react";
-import Form from "react-bootstrap/Form";
-import "../scss/homepage.scss";
+import React from 'react';
+import '../scss/homepage.scss';
 
 function Display(props) {
-  const { mood } = props;
-  console.log(mood[0]);
-  const loaded = () => (
-    <div className="Display">
-      <h1 className="moodName">{mood[0].name}</h1>
-      <button className="MoodFood">Add Your Mood Food</button>
-      {mood[0].foods.map((food) => {
-        return (
-          <div key={food._id}>
-            <h1>{food.name}</h1>
-            <img src={food.img} alt=""></img>
-            <button className="EditDelete">Edit</button>
-            <button className="EditDelete">Delete</button>
-          </div>
-        );
-      })}
-    </div>
-  );
-  const loading = <h1>Loading...</h1>;
-  return mood.length > 0 ? loaded() : loading;
+	// const url = 'http://localhost:4000/gouda';
+	const url = 'https://its-all-gouda-backend.herokuapp.com/gouda';
+	const { mood } = props;
+
+	//FUNCTION TO DELETE A Food Works but does not reload the page
+	// Maybe refactor and set mood.foods to state so that when something
+	// is deleted it will refresh
+	const deleteFood = (food) => {
+		fetch(url + '/foods/' + food._id, {
+			method: 'delete',
+		}).then((response) => {
+			loaded();
+		});
+	};
+
+	const loaded = () => (
+		<div className='Display'>
+			<h1 className='moodName'>{mood[0].name}</h1>
+			<button className='MoodFood'>Add Your Mood Food</button>
+			{mood[0].foods.map((food) => {
+				return (
+					<div key={food._id}>
+						<h1>{food.name}</h1>
+						<img src={food.img} alt=''></img>
+						<button className='EditDelete'>Edit</button>
+						<button
+							className='EditDelete'
+							onClick={() => {
+								deleteFood(food);
+							}}>
+							Delete
+						</button>
+					</div>
+				);
+			})}
+		</div>
+	);
+	const loading = <h1>Loading...</h1>;
+	return mood.length > 0 ? loaded() : loading;
 }
 
 export default Display;
--- src/components/form.js
+++ src/components/form.js
@@ -1,84 +1,85 @@
-import React, { useState, useEffect } from "react";
-import "../scss/form.scss";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import React, { useState } from 'react';
+import '../scss/form.scss';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function CreateForm(props) {
-  const [addFood, setAddFood] = useState({});
-  const [state, setState] = useState({
-    mood: "",
-    food: "",
-    img: "",
-    description: "",
-  });
+	// const url = 'http://localhost:4000/gouda';
+	const url = 'https://its-all-gouda-backend.herokuapp.com/gouda';
 
-  const handleChange = (event) => {
-    setState({ ...state, [event.target.name]: event.target.value });
-  };
+	const [state, setState] = useState({
+		mood: 'Happy',
+		food: '',
+		img: '',
+		description: '',
+	});
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setAddFood(state);
-  };
+	const handleChange = (event) => {
+		setState({ ...state, [event.target.name]: event.target.value });
+	};
 
-  return (
-    <div className="form">
-      <h1 className="addmood">Add Mood Food</h1>
-      <Form onSubmit={handleSubmit} className="form">
-        <Form.Group controlId="exampleForm.ControlSelect1">
-          <Form.Label>How you Feelin'?</Form.Label>
-          <br />
-          <Form.Control name="mood" as="select" onChange={handleChange}>
-            <option>Happy</option>
-            <option>Sad</option>
-            <option>Moody</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Control
-            type="text"
-            name="food"
-            placeholder="Type of Food"
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Control
-            type="text"
-            name="img"
-            placeholder="image link"
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Form.Group controlId="exampleForm.ControlInput1">
-          <Form.Control
-            type="text"
-            name="description"
-            placeholder="description"
-            onChange={handleChange}
-          />
-        </Form.Group>
-        <Button
-          className="mt-2"
-          type="submit"
-          variant="primary"
-          size="sm"
-          block
-        >
-          Submit
-        </Button>
-        <Button
-          className="mt-2"
-          type="submit"
-          variant="primary"
-          size="sm"
-          block
-        >
-          Back to Mood
-        </Button>
-      </Form>
-    </div>
-  );
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		props.handleSubmit(state);
+		props.history.push('/');
+	};
+
+	return (
+		<div className='form'>
+			<h1 className='addmood'>Add Mood Food</h1>
+			<Form onSubmit={handleSubmit} className='form'>
+				<Form.Group controlId='exampleForm.ControlSelect1'>
+					<Form.Label>How you Feelin'?</Form.Label>
+					<br />
+					<Form.Control name='mood' as='select' onChange={handleChange}>
+						<option>Happy</option>
+						<option>Blue</option>
+						<option>Moody</option>
+					</Form.Control>
+				</Form.Group>
+				<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Control
+						type='text'
+						name='food'
+						placeholder='Type of Food'
+						onChange={handleChange}
+					/>
+				</Form.Group>
+				<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Control
+						type='text'
+						name='img'
+						placeholder='image link'
+						onChange={handleChange}
+					/>
+				</Form.Group>
+				<Form.Group controlId='exampleForm.ControlInput1'>
+					<Form.Control
+						type='text'
+						name='description'
+						placeholder='description'
+						onChange={handleChange}
+					/>
+				</Form.Group>
+				<Button
+					className='mt-2'
+					type='submit'
+					variant='primary'
+					size='sm'
+					block>
+					Submit
+				</Button>
+				<Button
+					className='mt-2'
+					type='submit'
+					variant='primary'
+					size='sm'
+					block>
+					Back to Mood
+				</Button>
+			</Form>
+		</div>
+	);
 }
 
 export default CreateForm;
